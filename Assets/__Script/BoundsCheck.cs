adf91c479aeb5342343bd8b826d840a8079bda36
bcook03 <bnc121203@outlook.com>
2025-03-18 15:02:42 -0500

    added checkRadius

--- Assets/__Script/BoundsCheck.cs
+++ Assets/__Script/BoundsCheck.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
+    public enum eType {center, inset, outset};
+
+    [Header("Inscribed")]
+    public eType boundsType = eType.center;
+    public floar radius = 1f;
+
     [Header("Dynamic")]
     public float camWidth;
     public float camHeight;
@@ -16,19 +22,24 @@ public class BoundsCheck : MonoBehaviour
     }
 
     void LateUpdate() {
+        // Find the checkRadius that will enable center, inset, or outset
+        float checkRadius = 0;
+        if (boundsType == eType.inset) checkRadius = -radius;
+        if (boundsType == eType.outset) checkRadius = radius;
+
         Vector3 pos = transform.position;
 
         // restrict the X position to camWidth
-        if (pos.x > camWidth)
-            pos.x = camWidth;
-        if (pos.x < -camWidth)
-            pos.x = -camWidth;
+        if (pos.x > camWidth + checkRadius)
+            pos.x = camWidth + checkRadius;
+        if (pos.x < -camWidth - checkRadius)
+            pos.x = -camWidth - checkRadius;
         
         // Restrict the Y position to cmaHeight
-        if (pos.y > camHeight)
-            pos.y = camHeight;
-        if (pos.y < -camHeight)
-            pos.y = -camHeight;
+        if (pos.y > camHeight + checkRadius)
+            pos.y = camHeight + checkRadius;
+        if (pos.y < -camHeight - checkRadius)
+            pos.y = -camHeight - checkRadius;
 
         transform.position = pos;
     }
