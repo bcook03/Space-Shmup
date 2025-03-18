6bef94078cbf998ae0c20d5b841ee14b66e98dd6
bcook03 <bnc121203@outlook.com>
2025-03-17 19:29:28 -0500

    updated BoundCheck

--- Assets/__Script/BoundsCheck.cs
+++ Assets/__Script/BoundsCheck.cs
@@ -5,7 +5,34 @@ public class BoundsCheck : MonoBehaviour
     [Header("Dynamic")]
     public float camWidth;
     public float camHeight;
+    /// <summary>
+    /// Keeps a GameObject on screen.
+    /// Note that this ONLY works for an orhographic Main Camera
+    /// </summary>
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake() {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    void LateUpdate() {
+        Vector3 pos = transform.position;
+
+        // restrict the X position to camWidth
+        if (pos.x > camWidth)
+            pos.x = camWidth;
+        if (pos.x < -camWidth)
+            pos.x = -camWidth;
+        
+        // Restrict the Y position to cmaHeight
+        if (pos.y > camHeight)
+            pos.y = camHeight;
+        if (pos.y < -camHeight)
+            pos.y = -camHeight;
+
+        transform.position = pos;
+    }
+
     void Start()
     {
         
