e330764cdf25dafe454f8f37e4c2e8032d24aeff
bcook03 <bnc121203@outlook.com>
2025-03-19 19:27:57 -0500

    updated BoundCheck

--- Assets/__Script/BoundsCheck.cs
+++ Assets/__Script/BoundsCheck.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
+    [System.Flags]
+    public enum eScreenLocs {
+        onScreen = 0,
+        offRight = 1,
+        offLeft = 2,
+        offUp = 4,
+        offDown = 8
+    }
     public enum eType {center, inset, outset};
 
     [Header("Inscribed")]
@@ -10,9 +18,9 @@ public class BoundsCheck : MonoBehaviour
     public bool keepOnScreen = true;
 
     [Header("Dynamic")]
+    public eScreenLocs screenLocs = eScreenLocs.onScreen;
     public float camWidth;
     public float camHeight;
-    public bool isOnScreen = true;
     /// <summary>
     /// Keeps a GameObject on screen.
     /// Note that this ONLY works for an orhographic Main Camera
@@ -30,31 +38,41 @@ public class BoundsCheck : MonoBehaviour
         if (boundsType == eType.outset) checkRadius = radius;
 
         Vector3 pos = transform.position;
-        isOnScreen = true;
+        screenLocs = eScreenLocs.onScreen;
 
         // restrict the X position to camWidth
         if (pos.x > camWidth + checkRadius){
             pos.x = camWidth + checkRadius;
-            isOnScreen = false;
+            screenLocs |= eScreenLocs.offRight;
         }
         if (pos.x < -camWidth - checkRadius){
             pos.x = -camWidth - checkRadius;
-            isOnScreen = false;
+            screenLocs |= eScreenLocs.offLeft;
         }
         // Restrict the Y position to cmaHeight
         if (pos.y > camHeight + checkRadius){
             pos.y = camHeight + checkRadius;
-            isOnScreen = false;
+            screenLocs |= eScreenLocs.offUp;
         }
         if (pos.y < -camHeight - checkRadius){
             pos.y = -camHeight - checkRadius;
-            isOnScreen = false;
+            screenLocs |= eScreenLocs.offDown;
         }
 
         if (keepOnScreen && !isOnScreen){
             transform.position = pos;
-            isOnScreen = true;
+            screenLocs |= eScreenLocs.onScreen;
         }
+    }
+
+    public bool isOnScreen {
+        get { return (screenLocs == eScreenLocs.onScreen); }
+    }
+
+    public bool LocIs( eScreenLocs checkLoc ){
+        if( checkLoc == eScreenLocs.onScreen) return isOnScreen; 
+        return ( (screenLocs & checkLoc) == checkLoc );
+        
     }
 
 }
--- Assets/__Script/Enemy.cs
+++ Assets/__Script/Enemy.cs
@@ -33,11 +33,8 @@ public class Enemy : MonoBehaviour
         Move();
 
         // Check whether this Enemy has gone off the bottom of the screen
-        if(!bndCheck.isOnScreen) {
-            if (pos.y < bndCheck.camHeight - bndCheck.radius) {
-                // We're off the bottom, so destroy this GameObject
-                Destroy(gameObject);
-            }
+        if (bndCheck.LocIs( BoundsCheck.eScreenLocs.offDown)) {
+            Destroy(gameObject);
         }
     }
 
