ac08de529d5e31f0a8b5c6ba7c4d0721b32fd453
bcook03 <bnc121203@outlook.com>
2025-03-19 20:17:11 -0500

    Taking damage

--- Assets/__Script/Hero.cs
+++ Assets/__Script/Hero.cs
@@ -12,8 +12,10 @@ public class Hero : MonoBehaviour
     public float rollMult = -45;
     public float pitchMult = 30;
 
-    [Header("Dynamic")] [Range(0,4)]
-    public float shieldLevel = 1;
+    [Header("Dynamic")] [Range(0,4)] [SerializeField]
+    private float _shieldLevel = 1;
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null;
 
     void Awake() {
         if (S == null) {
@@ -39,5 +41,34 @@ public class Hero : MonoBehaviour
 
         // Rotatte the ship to make it feel more dynamic
         transform.rotation = Quaternion.Euler(vAxis*pitchMult,hAxis*pitchMult,0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Transform rootT = other.gameObject.transform.root;
+        GameObject go = rootT.gameObject;
+        
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy != null){
+            shieldLevel--;
+            Destroy(go);
+        }
+        else {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: "+go.name);
+        }
+    }
+
+    public float shieldLevel {
+        get { return (_shieldLevel); }
+        private set {
+            _shieldLevel = Mathf.Min(value, 4);
+            // If the shield is going to be set to less than zero...
+            if (value < 0) {
+                Destroy(this.gameObject); // Destroy the Hero
+            }
+        }
     }
 }
---
+++ Assets/__Script/Main.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour
+{
+    static private Main S;
+
+    [Header("Inscribed")]
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemyInsetDefault = 1.5f;
+
+    private BoundsCheck bndCheck;
+
+    void Awake()
+    {
+        S = this;
+        // Set bndCheck to reference the BoundsCheck component on this GameObject
+        bndCheck = GetComponent<BoundsCheck>();
+
+        // Invokde SpawnEnmy() once (in 2 seconds, based on default values)
+        Invoke( nameof(SpawnEnemy), 1f/enemySpawnPerSecond);    
+    }
+
+    public void SpawnEnemy() {
+        // Pick a random Enemy prefab to instantiate
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate<GameObject>(prefabEnemies[ndx]);
+
+        // Position the enemey above the screen with a random x position;
+        float enemyInset = enemyInsetDefault;
+        if(go.GetComponent<BoundsCheck>() != null) {
+            enemyInset = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        // Set the initial position for the spawned Enemy
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camWidth + enemyInset;
+        float xMax = bndCheck.camWidth - enemyInset;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyInset;
+        go.transform.position = pos;
+
+        Invoke(nameof(SpawnEnemy), 1f/enemySpawnPerSecond);
+    }
+}
